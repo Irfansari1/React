b912df575d97d90e70adbeeb312e5d369559c545
Irfansari1 <irfansari42@gmail.com>
2022-01-12 22:47:43 +0100

    updating the status added

--- projects/basic-counter/src/components/counter.js
+++ projects/basic-counter/src/components/counter.js
@@ -3,46 +3,28 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
-    imageUrl: "https://picsum.photos/400",
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  /*   styles = {
-    fontSize: 30,
-    fontWeight: "bold",
-  }; */
+  handleIncrement = () => {
+    //this.state.count++;
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
-      //className="d-flex flex-column justify-content-center align-items-center m-2"
-      // && bundan dolayi boolean ve string karsilatiriliyor ve ilk kisim true ikinci kisim truthy ise ikincisini seciyor...
-      //condition imizi yazdirdik ardindan sonuc
       <div>
-        {this.state.tags.length === 0 && "Plesae create a new tag!"}
-        {this.renderTags()}
-        {/* <img
-          className="rounded mx-auto d-block"
-          src={this.state.imageUrl}
-          alt=""
-        /> */}
-        {/*   <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
-  /*   getBadgeClasses() {
+  getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
@@ -51,10 +33,7 @@ class Counter extends Component {
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
-  } */
+  }
 }
 
 export default Counter;
-
-//we can write any js expression {2+2}
-//instead of div we can use '<React.Fragment>''
